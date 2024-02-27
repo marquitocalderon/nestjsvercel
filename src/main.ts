48c43abf0a368596595ce041706a2a30c320f16b
Marco Calderon <marco.calderon1701@hotmail.com>
2024-02-27 14:12:13 -0500

    con fe sswagger

--- src/main.ts
+++ src/main.ts
@@ -6,24 +6,30 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('DOCUMENTACION')
-    .setDescription('API Documentation SOLICITUDES')
-    .setVersion('1.0')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'APIKey',
-        name: 'APIKey',
-        description: 'Enter API Key',
-        in: 'header',
-      },
-      'APIKey-auth', 
-    )
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+ // swagger setup
+ const config = new DocumentBuilder()
+ .setTitle('Backend Generator')
+ .setDescription('Documentation API Test')
+ .setVersion('1.0')
+ .setBasePath('api/v1')
+ .addBearerAuth({ type: 'http', scheme: 'bearer', bearerFormat: 'JWT' })
+ .build();
+
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('swagger', app, document, {
+ customSiteTitle: 'Backend Generator',
+ customfavIcon: 'https://avatars.githubusercontent.com/u/6936373?s=200&v=4',
+ customJs: [
+   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+ ],
+ customCssUrl: [
+   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
+   'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+ ],
+});
+
 
   app.enableCors();
   app.useGlobalPipes(
