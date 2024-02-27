549fe1cfe8df89b2d64a8ee81d9f1412e361d445
Marco Calderon <marco.calderon1701@hotmail.com>
2024-02-27 12:30:14 -0500

    la fee

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerUIBundle } from 'swagger-ui-dist';  // Importa Swagger UI
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('DOCUMENTACION')
+    .setDescription('API Documentation SOLICITUDES')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      // Aquí puedes configurar opciones de Swagger UI si es necesario
+      // En este caso, especificamos el bundle de Swagger UI que importamos
+      swaggerUI: SwaggerUIBundle,
+    },
+  });
+
   app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,16 +33,7 @@ async function bootstrap() {
     })
   );
 
-  const config = new DocumentBuilder()
-    .setTitle('DOCUMENTACION')
-    .setDescription('API Documentation SOLICITUDES')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
-
   await app.listen(4000);
-
 }
+
 bootstrap();
