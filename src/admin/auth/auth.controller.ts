b086ec638422410f7c037a8898b30e2d6136115c
Marco Calderon <marco.calderon1701@hotmail.com>
2024-02-27 14:38:55 -0500

    gaa

--- src/admin/auth/auth.controller.ts
+++ src/admin/auth/auth.controller.ts
@@ -19,7 +19,6 @@ export class AuthController {
     }
 
     @UseGuards(AuthGuard, RolesGuard)
-    @ApiExcludeEndpoint()
     @Post('refresh')
     refreshToken(@Body() datosFronted: RefreshTokenDTO) {
         return this.authService.generarToken_Con_REFRESH_TOKEN(datosFronted)
