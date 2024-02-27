22a1080300054132889914dd5c8c0bdc0f0f6d24
Marco Calderon <marco.calderon1701@hotmail.com>
2024-02-27 12:35:05 -0500

    la fe

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { SwaggerUIBundle, SwaggerUIStandalonePreset } from 'swagger-ui-dist';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,16 +12,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
-
-  SwaggerModule.setup('docs', app, document, {
-    swaggerOptions: {
-      // Utiliza los presets de SwaggerUIBundle
-      presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
-      // Otras opciones si es necesario
-    },
-  });
+  SwaggerModule.setup('docs', app, document);
 
   app.enableCors();
   app.useGlobalPipes(
@@ -34,6 +25,6 @@ async function bootstrap() {
   );
 
   await app.listen(4000);
-}
 
+}
 bootstrap();
